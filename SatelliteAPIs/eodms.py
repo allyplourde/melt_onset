dd4ce4acfed0f3427165ac382538791370ff5c16
Ally <allyplourde@gmail.com>
2021-02-22 09:52:11 -0700

    update eodms.py query tool

--- SatelliteAPIs/eodms.py
+++ SatelliteAPIs/eodms.py
@@ -9,13 +9,14 @@ import xml.etree.ElementTree as ET
 import getpass
 
 # GetRecords POST request 
-def getRecords(max_records, end_date, lower_corner, upper_corner):
+def getRecords(max_records, start_date, end_date, lower_corner, upper_corner):
     # Submit a GetRecords to the CSW
 
     # Step 1: Generate xml request
+   # Submit a GetRecords to the CSW
     post_xml = '''<?xml version="1.0" encoding="UTF-8"?>
     <csw:GetRecords service='CSW' version='2.0.2'
-        maxRecords='%s'
+        maxRecords='15'
         startPosition='1'
         resultType='results'
         outputFormat='application/xml'
@@ -31,44 +32,46 @@ def getRecords(max_records, end_date, lower_corner, upper_corner):
         xsi:schemaLocation='http://www.opengis.net/cat/csw/2.0.2
         http://schemas.opengis.net/csw/2.0.2/CSW-discovery.xsd'>
         <csw:Query typeNames='csw:Record'>
-            <csw:ElementSetName typeNames='csw:Record'>full</csw:elementsetname>
+            <csw:ElementSetName typeNames='csw:Record'>full</csw:ElementSetName>
             <csw:Constraint version="1.1.0">
                 <ogc:Filter>
                     <ogc:And>
                         <ogc:PropertyIsLessThan>
-                            <ogc:PropertyName>dc:date</ogc:propertyname>
-                            <ogc:Literal>%s</ogc:literal>
-                        </ogc:propertyislessthan>
-                        <ogc:PropertyIsLike escapeChar='\\' singleChar='?' 
-                            wildCard='*'>
-                            <ogc:PropertyName>dc:title</ogc:propertyname>
-                            <ogc:Literal>*</ogc:literal>
-                        </ogc:propertyislike>
+                            <ogc:PropertyName>dc:date</ogc:PropertyName>
+                            <ogc:Literal>%s</ogc:Literal>
+                        </ogc:PropertyIsLessThan>
+                        <ogc:PropertyIsGreaterThan>
+                            <ogc:PropertyName>dc:date</ogc:PropertyName>
+                            <ogc:Literal>%s</ogc:Literal>
+                        </ogc:PropertyIsGreaterThan>
+                        <ogc:PropertyIsLike escapeChar='\\' singleChar='?' wildCard='*'>
+                            <ogc:PropertyName>dc:title</ogc:PropertyName>
+                            <ogc:Literal>*</ogc:Literal>
+                        </ogc:PropertyIsLike>
                         <ogc:BBOX>
-                            <ogc:PropertyName>ows:BoundingBox</ogc:propertyname>
+                            <ogc:PropertyName>ows:BoundingBox</ogc:PropertyName>
                             <gml:Envelope>
-                                <gml:lowerCorner>%s</gml:lowercorner>
-                                <gml:upperCorner>%s</gml:uppercorner>
-                            </gml:envelope>
-                        </ogc:bbox>
-                    </ogc:and>
-            </ogc:filter>
-            </csw:constraint>
-        </csw:query>
-    </csw:getrecords>''' % (max_records, end_date, lower_corner, upper_corner)
+                                <gml:lowerCorner>%s</gml:lowerCorner>
+                                <gml:upperCorner>%s</gml:upperCorner>
+                            </gml:Envelope>
+                        </ogc:BBOX>
+                    </ogc:And>
+            </ogc:Filter>
+            </csw:Constraint>
+        </csw:Query>
+    </csw:GetRecords>''' % (end_date, start_date, lower_corner, upper_corner)
 
     # Step 2: Send request
 
-    csw_url = 'https://www.eodms-sgdot.nrcan-rncan.gc.ca/MetaManagerCSW' \
-                '/csw/eodms_catalog'
+    csw_url = 'https://www.eodms-sgdot.nrcan-rncan.gc.ca/MetaManagerCSW/csw/eodms_catalog'
     headers = {'Content-Type':'application/xml'}
-    csw_r = requests.post(csw_url, data=post_xml, headers=headers)
+    csw_r = requests.post(csw_url, data=post_xml)
 
     root = ET.fromstring(csw_r.content)
 
     print(csw_r.content)
 
-    record_tag = '{http://www.opengis.net/cat/csw/2.0.2}SummaryRecord'
+    record_tag = '{http://www.opengis.net/cat/csw/2.0.2}Record'
     rec_element = []
     for child in root.iter("*"):
         if child.tag == record_tag:
@@ -101,17 +104,17 @@ if __name__ == "__main__":
     #set to true to order imagery,
     #link will be sent via email and
     # will be avaible on the EODMS 
-    submit_order = True
+    submit_order = False
 
     #Coordinate for Admiralty Inlet
-    bounding_box = {'lower_corner': '-85, 73.6', #lng-lat, string input
-                    'upper_corner': '-84.23, 73'}
+    bounding_box = {'lower_corner': '-85.000000 73.000000', #lng-lat, string input
+                    'upper_corner': '-84.230000 73.600000'}
 
-    date_range = [None, "2013-03-29Z"] #start date, end date, "YYYY-MM-DDZ"
+    date_range = ["2017-04-01Z", "2017-07-31Z"] #start date, end date, "YYYY-MM-DDZ"
 
     maxRecords = 10
 
-    records = getRecords(maxRecords, date_range[1], bounding_box['lower_corner'], bounding_box['upper_corner'])
+    records = getRecords(maxRecords, date_range[0], date_range[1], bounding_box['lower_corner'], bounding_box['upper_corner'])
     print("Number of Records: ", len(records))
     query = {
         "destinations": [], 
